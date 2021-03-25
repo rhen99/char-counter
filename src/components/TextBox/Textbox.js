cea33ece7cd0b3adef0698abed6fc3afbc1d590d
Arhen Santiago <lincoldarhen9900@gmail.com>
2021-03-25 15:27:15 +0800

    char counter

--- src/components/TextBox/Textbox.js
+++ src/components/TextBox/Textbox.js
@@ -3,7 +3,7 @@ function Textbox({changeCount}) {
     return (
         <section className="TextBox">
             <div className="container Container">
-                <textarea onChange={(e) => changeCount(e.target.value.length)} autoFocus placeholder="Write / Paste your text here..." className="TextBoxInput" ></textarea>
+                <textarea onChange={(e) => changeCount(e.target.value.length)} autoFocus placeholder="Write / Paste your text here..." className="TextBoxInput"></textarea>
             </div>
         </section>
     )
